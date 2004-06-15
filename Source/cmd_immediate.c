5874e26510a67723bff6c61c0f7de1cd648cfab1
Ken Weinert <madcoder42@users.sourceforge.net>
2004-06-15 20:26:55 +0000

    Due to programmer head space (and cut/paste coding) this function was checking for both normal and final orders, so it would run a turn as soon as everyone had turned in *any* type of orders.
    This has now been fixed, it only checks for final orders.

--- Source/cmd_immediate.c
+++ Source/cmd_immediate.c
@@ -75,13 +75,6 @@ int CMD_immediate( int argc, char **argv) {
 										  aPlayer->name, aGame->turn+1);
 				
 				no_orders = access(ordersfile, R_OK);
-				if (no_orders) {
-					free(ordersfile);
-					ordersfile = createString("%s/orders/%s/%s.%d",
-										  galaxynghome, argv[2],
-										  aPlayer->name, aGame->turn+1);
-					no_orders = access(ordersfile, R_OK);	
-				}
 				
 				if (no_orders) {
 					char* notify_file;
@@ -148,7 +141,7 @@ int CMD_immediate( int argc, char **argv) {
 				eMail(aGame, env, gmbody);
 				destroyEnvelope(env);
 				plog( LPART, "Not all orders in, skipping tick.\n");
-				
+				return result;
 			}
 			
 			
