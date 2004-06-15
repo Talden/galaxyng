95db1ad8841e0bac9305ec75e9c79c1a5a96e548
Ken Weinert <madcoder42@users.sourceforge.net>
2004-06-15 20:39:46 +0000

    temporarily turned off requirement for a turn number in orders

--- Source/cmd_check.c
+++ Source/cmd_check.c
@@ -274,7 +274,7 @@ int CMD_check( int argc, char **argv, int kind ) {
 		
 		setHeader( anEnvelope, MAILHEADER_TO, "%s", returnAddress );
 		
-		if ( resNumber == RES_OK ) {
+		if ( resNumber == RES_OK || resNumber == RES_NO_TURN_NBR) {
 			aPlayer = findElement( player, aGame->players, raceName );
 			aPlayer->orders = NULL;
 			
