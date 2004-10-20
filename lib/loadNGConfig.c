23c6ff59c67f6392765341e04eb8fef265386d72
Ken Weinert <madcoder42@users.sourceforge.net>
2004-10-20 13:15:43 +0000

    changed to allow for game specific .galaxyngrc files to supplement the main .galaxyngrc file. current behaviour will still work, but you can now put into the game specific files *only* those items you need to override from the main config file

--- lib/loadNGConfig.c
+++ lib/loadNGConfig.c
@@ -14,10 +14,9 @@
  ******
  */
 
-void readDefaults(game *aGame, FILE * f) {
-  char* isRead;			/* to check for EOF */
-  char* key;			/* the key part of the name/value pair */
-	
+void
+initServerOptions(game* aGame)
+{
   /* initialize values */
   aGame->serverOptions.sendmail = NULL;
   aGame->serverOptions.encode = NULL;
@@ -31,49 +30,80 @@ void readDefaults(game *aGame, FILE * f) {
   aGame->serverOptions.fontpath = NULL;
   aGame->serverOptions.due = NULL;
   aGame->serverOptions.tick_interval = NULL;
-  
+}
+
+void readDefaults(game *aGame, FILE * f) {
+  char* isRead;			/* to check for EOF */
+  char* key;			/* the key part of the name/value pair */
+	
   for (isRead = fgets(lineBuffer, LINE_BUFFER_SIZE, f);
        isRead; isRead = fgets(lineBuffer, LINE_BUFFER_SIZE, f)) {
     
     key = getstr(lineBuffer);
     if (key[0] != '\0') {
       if (noCaseStrcmp("GMemail", key) == 0) {
+	if (aGame->serverOptions.GMemail)
+	  free(aGame->serverOptions.GMemail);
 	aGame->serverOptions.GMemail = strdup(getstr(0));
       }
       else if (noCaseStrcmp("GMname", key) == 0) {
+	if (aGame->serverOptions.GMname)
+	  free(aGame->serverOptions.GMname);
 	aGame->serverOptions.GMname = strdup(getstr(0));
       }
       else if (noCaseStrcmp("GMpassword", key) == 0) {
+	if (aGame->serverOptions.GMpassword)
+	  free(aGame->serverOptions.GMpassword);
 	aGame->serverOptions.GMpassword = strdup(getstr(0));
       }
       else if (noCaseStrcmp("SERVERemail", key) == 0) {
+	if (aGame->serverOptions.SERVERemail)
+	  free(aGame->serverOptions.SERVERemail);
 	aGame->serverOptions.SERVERemail = strdup(getstr(0));
       }
       else if (noCaseStrcmp("SERVERname", key) == 0) {
+	if (aGame->serverOptions.SERVERname)
+	  free(aGame->serverOptions.SERVERname);
 	aGame->serverOptions.SERVERname = strdup(getstr(0));
       }
       else if (noCaseStrcmp("ReplyTo", key) == 0) {
+	if (aGame->serverOptions.ReplyTo)
+	  free(aGame->serverOptions.ReplyTo);
 	aGame->serverOptions.ReplyTo = strdup(getstr(0));
       }
       else if (noCaseStrcmp("compress", key) == 0) {
+	if (aGame->serverOptions.compress)
+	  free(aGame->serverOptions.compress);
 	aGame->serverOptions.compress = strdup(getstr(0));
       }
       else if (noCaseStrcmp("encode", key) == 0) {
+	if (aGame->serverOptions.encode)
+	  free(aGame->serverOptions.encode);
 	aGame->serverOptions.encode = strdup(getstr(0));
       }
       else if (noCaseStrcmp("sendmail", key) == 0) {
+	if (aGame->serverOptions.sendmail)
+	  free(aGame->serverOptions.sendmail);
 	aGame->serverOptions.sendmail = strdup(getstr(0));
       }
       else if (noCaseStrcmp("starttime", key) == 0) {
+	if (aGame->starttime)
+	  free(aGame->starttime);
 	aGame->starttime = strdup(getstr(0));
       }
       else if (noCaseStrcmp("fontpath", key) == 0) {
+	if (aGame->serverOptions.fontpath)
+	  free(aGame->serverOptions.fontpath);
 	aGame->serverOptions.fontpath = strdup(getstr(0));
       }
       else if (noCaseStrcmp("due", key) == 0) {
+	if (aGame->serverOptions.due)
+	  free(aGame->serverOptions.due);
 	aGame->serverOptions.due = strdup(getstr(0));
       }
       else if (noCaseStrcmp("tick", key) == 0) {
+	if (aGame->serverOptions.tick_interval)
+	  free(aGame->serverOptions.tick_interval);
 	aGame->serverOptions.tick_interval = strdup(getstr(0));
       }
       else {
@@ -124,19 +154,23 @@ void readDefaults(game *aGame, FILE * f) {
  ****
  */
 
-void loadNGConfig(game *aGame) {
-	FILE           *f;
-	
-	sprintf(lineBuffer, "%s/.galaxyngrc", galaxynghome);
-	if ((f = fopen(lineBuffer, "r"))) {
-		readDefaults(aGame, f);
-		fclose(f);
-	}
-	sprintf(lineBuffer, "%s/data/%s/.galaxyngrc", galaxynghome, aGame->name);
-	if ((f = fopen(lineBuffer, "r"))) {
-		readDefaults(aGame, f);
-		fclose(f);
-	}
+void
+loadNGConfig(game *aGame) {
+  FILE *f;
+
+  initServerOptions(aGame);
+
+  sprintf(lineBuffer, "%s/.galaxyngrc", galaxynghome);
+  if ((f = fopen(lineBuffer, "r"))) {
+    readDefaults(aGame, f);
+    fclose(f);
+  }
+
+  sprintf(lineBuffer, "%s/data/%s/.galaxyngrc", galaxynghome, aGame->name);
+  if ((f = fopen(lineBuffer, "r"))) {
+    readDefaults(aGame, f);
+    fclose(f);
+  }
 }
 
 
