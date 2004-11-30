47eac13236405168feaceded96de94cd59d49412
Ken Weinert <madcoder42@users.sourceforge.net>
2004-11-30 01:07:49 +0000

    testing commit for "No report 1 returned" bug

--- Source/cmd_report.c
+++ Source/cmd_report.c
@@ -39,8 +39,9 @@ CMD_report( int argc, char **argv ) {
     anEnvelope = createEnvelope(  );
     returnAddress = getReturnAddress( stdin );
 
-    plog( LBRIEF, "Report request from %s.\n", returnAddress );
     theTurnNumber = getTurnNumber( stdin );
+    plog(LBRIEF, "Report request from %s for turn %d.\n",
+		 returnAddress, theTurnNumber);
     raceName = NULL;
     password = NULL;
     final_orders = NULL;
@@ -49,13 +50,15 @@ CMD_report( int argc, char **argv ) {
       areValidOrders( stdin, &aGame, &raceName, &password,
 		      &final_orders, &theTurnNumber );
     
-    reportName = createString("%s/temp_report_copy_%s", tempdir, returnAddress);
+    reportName = createString("%s/temp_report_copy_%d_%s",
+							  tempdir, theTurnNumber, returnAddress);
+	plog(LBRIEF, "Report is in \"%s\"\n", reportName);
     {
       char *ptr = strrchr(reportName, '/')+1;
       while (*ptr) {
-	if (!isalnum(*ptr))
-	  *ptr = '_';
-	ptr++;
+		  if (!isalnum(*ptr))
+			  *ptr = '_';
+		  ptr++;
       }
     }
     
@@ -68,63 +71,63 @@ CMD_report( int argc, char **argv ) {
     report = fopen(reportName, "w");
     
     if ( ( resNumber == RES_TURNRAN ) ||
-	 ( ( resNumber == RES_OK ) &&
-	   ( theTurnNumber == LG_CURRENT_TURN ) ) ) {
-      game *aGame2;
+		 ( ( resNumber == RES_OK ) &&
+		   ( theTurnNumber == LG_CURRENT_TURN ) ) ) {
+		game *aGame2;
       
-      if ( theTurnNumber > 0 ) {
-		  aGame2 = loadgame( aGame->name, theTurnNumber - 1 );
-      }
-      else if ( theTurnNumber == LG_CURRENT_TURN ) {
-		  theTurnNumber = aGame->turn;
-		  aGame2 = loadgame( aGame->name, theTurnNumber - 1 );
-      }
-      else {
-		  aGame2 = loadgame( aGame->name, 0 );
-      }
+		if ( theTurnNumber > 0 ) {
+			aGame2 = loadgame( aGame->name, theTurnNumber - 1 );
+		}
+		else if ( theTurnNumber == LG_CURRENT_TURN ) {
+			theTurnNumber = aGame->turn;
+			aGame2 = loadgame( aGame->name, theTurnNumber - 1 );
+		}
+		else {
+			aGame2 = loadgame( aGame->name, 0 );
+		}
       
-      if ( aGame2 ) {
-		  player *aPlayer;
-		  int index;
-	
-		  loadNGConfig( aGame2 );
-		  setHeader( anEnvelope, MAILHEADER_SUBJECT,
-					 "[GNG] %s turn %d report copy for %s",
-					 aGame->name, theTurnNumber, raceName );
-		  if ( theTurnNumber > 0 ) {  /* Rerun the turn */
-			  char *ordersName;
-			  
-			  ordersName =
-				  createString( "%s/orders/%s/%d.all",
-								galaxynghome, aGame2->name,
-								theTurnNumber );
-			  runTurn( aGame2, ordersName );
-			  free( ordersName );
-		  }
-		  /* Translate the current race name into the name used
-			 during the turn that is requested
-		  */
-		  aPlayer =
-			  findElement( player, aGame->players, raceName );
-		  
-		  index = ptonum( aGame->players, aPlayer );
-		  aPlayer = numtop( aGame2->players, index );
-		  
-		  if (aPlayer->flags & F_COMPRESS)
-			  anEnvelope->compress = TRUE;
-		  
-		  if ( theTurnNumber == 0 )
-			  aPlayer->pswdstate = 1;
-		  highScoreList( aGame2 );
-		  createTurnReport( aGame2, aPlayer, report, 0 );
-      }
-      else {
-		  setHeader( anEnvelope, MAILHEADER_SUBJECT,
-					 "[GNG] %s turn %d report copy for %s",
-					 aGame->name, theTurnNumber, raceName  );
-		  fprintf( report,
-				   "\n\nThe turn you requested is no longer available...\n" );
-      }
+		if ( aGame2 ) {
+			player *aPlayer;
+			int index;
+			
+			loadNGConfig( aGame2 );
+			setHeader( anEnvelope, MAILHEADER_SUBJECT,
+					   "[GNG] %s turn %d report copy for %s",
+					   aGame->name, theTurnNumber, raceName );
+			if ( theTurnNumber > 0 ) {  /* Rerun the turn */
+				char *ordersName;
+				
+				ordersName =
+					createString( "%s/orders/%s/%d.all",
+								  galaxynghome, aGame2->name,
+								  theTurnNumber );
+				runTurn( aGame2, ordersName );
+				free( ordersName );
+			}
+			/* Translate the current race name into the name used
+			   during the turn that is requested
+			*/
+			aPlayer =
+				findElement( player, aGame->players, raceName );
+			
+			index = ptonum( aGame->players, aPlayer );
+			aPlayer = numtop( aGame2->players, index );
+			
+			if (aPlayer->flags & F_COMPRESS)
+				anEnvelope->compress = TRUE;
+			
+			if ( theTurnNumber == 0 )
+				aPlayer->pswdstate = 1;
+			highScoreList( aGame2 );
+			createTurnReport( aGame2, aPlayer, report, 0 );
+		}
+		else {
+			setHeader( anEnvelope, MAILHEADER_SUBJECT,
+					   "[GNG] %s turn %d report copy for %s",
+					   aGame->name, theTurnNumber, raceName  );
+			fprintf( report,
+					 "\n\nThe turn you requested is no longer available...\n" );
+		}
     }
     else if ( resNumber == RES_OK ) {
 		setHeader( anEnvelope, MAILHEADER_SUBJECT,
@@ -149,7 +152,6 @@ CMD_report( int argc, char **argv ) {
     fclose( report );
     result = eMail( aGame, anEnvelope, reportName );
     destroyEnvelope( anEnvelope );
-    fprintf(stderr, "Report in %s\n", reportName);
     result |= ssystem( "rm %s", reportName );
     result = ( result ) ? EXIT_FAILURE : EXIT_SUCCESS;
     if ( raceName )
