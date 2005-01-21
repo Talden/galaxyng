a6d77825fb7d5544e950823589d5ae35b9171949
Ken Weinert <madcoder42@users.sourceforge.net>
2005-01-21 04:08:28 +0000

    fixes for outputting report instead of saving as a file

--- Source/cmd_webcheck.c
+++ Source/cmd_webcheck.c
@@ -26,7 +26,6 @@ CMD_webcheck( int argc, char **argv, int kind) {
   int result;
   char *logName;
   char *forecastName;
-  char *returnAddress;
   char *raceName;
   char *password;
   char *final_orders;
@@ -56,36 +55,24 @@ CMD_webcheck( int argc, char **argv, int kind) {
       aPlayer = findElement(player, aGame->players, raceName);
       aPlayer->orders = NULL;
       
-      plog(LBRIEF, "Orders from %s\n", returnAddress);
+      plog(LBRIEF, "Orders from %s\n", raceName);
       
       /* produce an XML forecast */
       if (aPlayer->flags & F_XMLREPORT) {
 	if ((theTurnNumber == LG_CURRENT_TURN) ||
 	     (theTurnNumber == (aGame->turn) + 1)) {
-	  forecastName = createString("%s/NG_XML_%d_forecast",
-				       tempdir, getpid());
+
 	  copyOrders(aGame, stdin, raceName, password, final_orders,
 		      aGame->turn + 1);
-	  if ((forecast = fopen(forecastName, "w")) == NULL) {
-	    plog(LBRIEF, "Could not open %s for forecasting\n",
-		  forecastName);
-	    fprintf(stderr,"Could not open %s for forecasting\n",
-		    forecastName);
-	    return EXIT_FAILURE;
-	  }
 	  
-	  checkOrders(aGame, raceName, forecast, F_XMLREPORT);
+	  checkOrders(aGame, raceName, stdout, F_XMLREPORT);
 	  
-	  fclose(forecast);
-	  if (kind == CMD_CHECK_REAL) {
-	    printf("%s", forecastName);
-	  }
-	  else {
+	  if (kind != CMD_CHECK_REAL) {
 	    char *forecastFile;
 	    forecastFile =
 	      createString("%s/forecasts/%s/%s_XML",
 			    galaxynghome, argv[2],
-			    returnAddress);
+			    raceName);
 	    GOS_copy(forecastName, forecastFile);
 	  }
 	  result |= GOS_delete(forecastName);
@@ -97,58 +84,23 @@ CMD_webcheck( int argc, char **argv, int kind) {
       if (aPlayer->flags & F_TXTREPORT) {
 	if ((theTurnNumber == LG_CURRENT_TURN) ||
 	     (theTurnNumber == (aGame->turn) + 1)) {
-	  forecastName = createString("%s/NG_TXT_%d_forecast",
-				       tempdir, getpid());
-	  if ((forecast = fopen(forecastName, "w")) == NULL) {
-	    plog(LBRIEF, "Could not open %s for forecasting\n",
-		  forecastName);
-	    return EXIT_FAILURE;
-	  }
 	  
 	  if (aPlayer->orders == NULL)
 	    copyOrders(aGame, stdin, raceName, password, final_orders,
 			aGame->turn + 1);
 	  
-	  checkOrders(aGame, raceName, forecast, F_TXTREPORT);
+	  checkOrders(aGame, raceName, stdout, F_TXTREPORT);
 	  
-	  fclose(forecast);
-	  
-	  if (kind == CMD_CHECK_REAL) {
-	    printf("%s", forecastName);
-	  } else {
-	    char *forecastFile;
-	    forecastFile = createString("%s/forecasts/%s/%s_TXT",
-					galaxynghome, argv[2],
-					returnAddress);
-	    GOS_copy(forecastName, forecastFile);
-	  }
-	  result |= GOS_delete(forecastName);
-	  free(forecastName);
 	}
       }
       
     }
     else {
-      forecastName = createString("%s/NG_TXT_%d_errors",
-				   tempdir, getpid());
-      forecast = fopen(forecastName, "w");
       plog(LBRIEF, "Major Trouble %d\n", resNumber);
       
       generateErrorMessage(resNumber, aGame, raceName,
-			    theTurnNumber, forecast);
-      fclose(forecast);
+			    theTurnNumber, stdout);
       
-      if (kind == CMD_CHECK_REAL) {
-	printf("%s", forecastName);
-      } else {
-	char *forecastFile;
-	forecastFile =
-	  createString("%s/forecasts/%s/%s_ERR",
-			galaxynghome, argv[2], returnAddress);
-	GOS_copy(forecastName, forecastFile);
-	printf("%s", forecastName);
-      }
-      free(forecastName);
     }
     
     /* code here for advanced orders, we need to see how to determine this */
@@ -168,65 +120,34 @@ CMD_webcheck( int argc, char **argv, int kind) {
       }
       
       if (aPlayer->flags & F_XMLREPORT) {
-	forecastName = createString("%s/NG_XML_forecast", tempdir);
-	forecast = fopen(forecastName, "w");
 	
-	fprintf(forecast,
+	fprintf(stdout,
 		 "<galaxy>\n  <variant>GalaxyNG</variant>\n");
-	fprintf(forecast, "  <version>%d.%d.%d</version>\n",
+	fprintf(stdout, "  <version>%d.%d.%d</version>\n",
 		 GNG_MAJOR, GNG_MINOR, GNG_RELEASE);
-	fprintf(forecast, "  <game name=\"%s\">\n", aGame->name);
-	fprintf(forecast, "    <turn num=\"%d\">\n", theTurnNumber);
-	fprintf(forecast, "      <race name=\"%s\">\n", raceName);
-	fprintf(forecast, "        <message>\n");
-	fprintf(forecast, "          <line num=\"1\">"
+	fprintf(stdout, "  <game name=\"%s\">\n", aGame->name);
+	fprintf(stdout, "    <turn num=\"%d\">\n", theTurnNumber);
+	fprintf(stdout, "      <race name=\"%s\">\n", raceName);
+	fprintf(stdout, "        <message>\n");
+	fprintf(stdout, "          <line num=\"1\">"
 		 "O wise leader, your orders for turn %d</line>",
 		 theTurnNumber);
-	fprintf(forecast, "          <line num=\"2\">"
+	fprintf(stdout, "          <line num=\"2\">"
 		 "have been received and stored.</line>");
-	fprintf(forecast, "        </message>\n");
-	fprintf(forecast, "      </race>\n");
-	fprintf(forecast, "    </turn>\n");
-	fprintf(forecast, "  </game>\n");
-	fprintf(forecast, "</galaxy>\n");
-	fclose(forecast);
-	if (kind == CMD_CHECK_REAL) {
-	  printf("%s", forecastName);
-	} else {
-	  char *forecastFile;
-	  
-	  forecastFile =
-	    createString("%s/forecasts/%s/%s_XML",
-			  galaxynghome, argv[2], returnAddress);
-	  GOS_copy(forecastName, forecastFile);
-	}
-	result |= GOS_delete(forecastName);
-	free(forecastName);
-      }
-      
+	fprintf(stdout, "        </message>\n");
+	fprintf(stdout, "      </race>\n");
+	fprintf(stdout, "    </turn>\n");
+	fprintf(stdout, "  </game>\n");
+	fprintf(stdout, "</galaxy>\n");
+     }
+ 
       if (aPlayer->flags & F_TXTREPORT) {
 	if (aPlayer->orders == NULL)
 	  copyOrders(aGame, stdin, raceName, password, final_orders,
 		      theTurnNumber);
-	forecastName = createString("%s/NG_TXT_forecast", tempdir);
-	forecast = fopen(forecastName, "w");
-	fprintf(forecast, "O wise leader your orders for turn %d "
+	fprintf(stdout, "O wise leader your orders for turn %d "
 		 "have been received and stored.\n", theTurnNumber);
-	fclose(forecast);
-	if (kind == CMD_CHECK_REAL) {
-	  printf("%s", forecastName);
-	}
-	else {
-	  char *forecastFile;
-	  
-	  forecastFile =
-	    createString("%s/forecasts/%s/%s_TXT",
-			  galaxynghome, argv[2], returnAddress);
-	  GOS_copy(forecastName, forecastFile);
-	}
 	
-	result |= GOS_delete(forecastName);
-	free(forecastName);
       }
     }
   }
